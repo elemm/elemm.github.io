504510a66504c0872add91d1eb8300a06dfa3f84
elemm <eglemmerman@gmail.com>
2026-01-19 16:48:47 +0000

    Deploying to gh-pages from @ elemm/elemm.github.io@46d1dcca9600442a79c72838ab339e3dd796c1b3 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,14 +9,7 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-cv",
-          title: "cv",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/cv/";
-          },
-        },{id: "dropdown-bookshelf",
+  },{id: "dropdown-bookshelf",
               title: "bookshelf",
               description: "",
               section: "Dropdown",
